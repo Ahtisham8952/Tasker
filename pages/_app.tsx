0104a51adc23de7201120c96c948ea3bfd66d48b
Ahtisham8952 <134730992+Ahtisham8952@users.noreply.github.com>
2023-10-20 14:21:39 +0500

    tsx rat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app'; // Import the AppProps type from 'next/app'
 import TaskerTheme from '../src/theme'; // Import your theme from the correct path
