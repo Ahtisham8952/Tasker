4ead05118a40076dacd3194129c7eee987775108
Ahtisham8952 <134730992+Ahtisham8952@users.noreply.github.com>
2023-10-20 15:50:55 +0500

    Changes Name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePageMain from '../src/components/app/Tasker/LandingPage/index';
+import HomePageMain from '../src/components/app/Tasker/LandingPage/HomePageMain';
 const Home: React.FC = () => {
   return (
     <>
